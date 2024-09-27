4d9b4f23295c4af50eaa9f1d8453f5d1ef90cf1b
Ihor Tkachenko <igortkachenko987@gmail.com>
2024-09-27 11:05:28 +0200

    BugFix1

--- src/test/java/Tests/NegativePostTest.java
+++ src/test/java/Tests/NegativePostTest.java
@@ -8,6 +8,7 @@ import Utils.DataHelper;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static io.restassured.RestAssured.given;
--- src/test/java/Tests/NegativeUserTest.java
+++ src/test/java/Tests/NegativeUserTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.Matchers.equalTo;
 public class NegativeUserTest extends BaseTestCase {
 
     @ParameterizedTest
+//    @CsvFileSource (resources = "src/test/resources/paramGoRestTest.csv", numLinesToSkip = 1)
     @ValueSource(strings = {"  ", ""})
     public void incorrectUserNameTest(String input) {
         User newUser = DataHelper.createUser();
